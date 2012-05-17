3eb7a83a5af8da61c1d1a9f1dddc5ca02fb72be0
siguremon <siguremon@gmail.com>
2012-05-18 03:51:13 +0900

    initial commit

---
+++ src/main/java/net/siguremon/antexample/AntExample.java
@@ -0,0 +1,7 @@
+package net.siguremon.antexample;
+
+public class AntExample {
+	public void main(String[] argv) {
+		System.out.println("Hello Ant");
+	}
+}
---
+++ src/test/java/net/siguremon/antexample/TestAntExample.java
@@ -0,0 +1,11 @@
+package net.siguremon.antexample;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestAntExample {
+	@Test
+	public void testSum() {
+		fail("not implemented");
+	}
+}
