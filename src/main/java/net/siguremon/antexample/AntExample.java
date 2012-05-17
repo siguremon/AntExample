b5b09ced931b20b0e13c12ac54e36fbbfec603e0
siguremon <siguremon@gmail.com>
2012-05-18 04:08:23 +0900

    add method sum to AntExample class and write test code.

--- src/main/java/net/siguremon/antexample/AntExample.java
+++ src/main/java/net/siguremon/antexample/AntExample.java
@@ -4,4 +4,8 @@ public class AntExample {
 	public void main(String[] argv) {
 		System.out.println("Hello Ant");
 	}
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
 }
--- src/test/java/net/siguremon/antexample/TestAntExample.java
+++ src/test/java/net/siguremon/antexample/TestAntExample.java
@@ -1,11 +1,13 @@
 package net.siguremon.antexample;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class TestAntExample {
 	@Test
 	public void testSum() {
-		fail("not implemented");
+		AntExample obj = new AntExample();
+		assertThat(obj.sum(1, 2), is(3));
 	}
 }
